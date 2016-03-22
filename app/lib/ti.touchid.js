e892ba55e48b38d348afa8b69e44bd60c7482668
Hans Knoechel <hknoechel@appcelerator.com>
2016-03-22 11:08:02 +0100

    Support Touch ID-based reset process

---
+++ app/lib/ti.touchid.js
@@ -0,0 +1,61 @@
+/**
+ * Ti.TouchID
+ *
+ * Summary:	Support native Touch ID with Hyperloop in Titanium Mobile.
+ * Author: 	Hans Knoechel | Appcelerator, Inc
+ * Date: 	03/22/2016
+ * Version:	0.1.0
+ * Example
+ *
+ *	var touchID = require("ti.touchid");
+ *	if (touchID.isSupported()) {
+ *		touchID.authenticate({
+ *			reason : "Please verify to reset all devices",
+ *			callback : function(e) {
+ * 				Ti.API.warn("Success? - " + e.success);
+ *				Ti.API.warn("Error? - " + e.error);
+ *				Ti.API.warn("Code? - " + e.code);
+ *			}
+ *		});
+ *	}
+ */
+
+var UIDevice = require("UIKit/UIDevice");
+var NSNumericSearch = require("Foundation").NSNumericSearch;
+var NSOrderedAscending = require("Foundation").NSOrderedAscending;
+var LAContext = require('LocalAuthentication/LAContext');
+var LocalAuthentication = require('LocalAuthentication/LocalAuthentication');
+var context = new LAContext();
+
+function isSupported() {
+	var currentOSSupported = UIDevice.currentDevice().systemVersion.compareOptions("8.0", NSNumericSearch) != NSOrderedAscending;
+	var touchIDSupported = context.canEvaluatePolicyError(LocalAuthentication.LAPolicyDeviceOwnerAuthenticationWithBiometrics);
+
+	return currentOSSupported && touchIDSupported;
+}
+
+function authenticate(args) {
+	var reason = args.reason || null;
+	var callback = args.callback || null;
+
+	if (!reason || !callback) {
+		Ti.API.error("Ti.TouchID: Please provide a valid 'reason' and 'callback' parameter.");
+		return;
+	}
+
+	context.evaluatePolicyLocalizedReasonReply(LocalAuthentication.LAPolicyDeviceOwnerAuthenticationWithBiometrics, reason, function(success, error) {		
+		var attrs = {
+			success : success
+		};
+
+		if (error) {
+			attrs["error"] = error.localizedDescription;
+			attrs["code"] = error.code;
+		}
+
+		callback(attrs);
+	});
+}
+
+exports.isSupported = isSupported;
+exports.authenticate = authenticate;
--- app/widgets/com.appcelerator.robot.settings/controllers/widget.js
+++ app/widgets/com.appcelerator.robot.settings/controllers/widget.js
@@ -1,10 +1,49 @@
-
 /**
  *  Constructor
  **/
 (function constructor() {
 
 })();
+
+function handleAction(e) {
+	var action = e.itemId;
+
+	switch(action) {
+	case "reset":
+		resetDevices();
+		break;
+	default:
+		Ti.API.warn("Unhandled action: " + action);
+	}
+	
+	$.listView.deselectItem(e.sectionIndex, e.itemIndex);
+}
+
+function resetDevices() {
+	
+	function reset() {
+		var devices = Alloy.Collections.instance("device");
+		devices.destroyAll();
+	}
+	
+	var touchID = require("ti.touchid");
+	if (touchID.isSupported()) {
+		touchID.authenticate({
+			reason : "Please verify to reset all devices",
+			callback : function(e) {
+				
+				e.success && reset();
+				
+				Ti.API.warn("Success? - " + e.success);
+				Ti.API.warn("Error? - " + e.error);
+				Ti.API.warn("Code? - " + e.code);
+			}
+		});
+	} else {
+		Ti.API.info("TouchID not supported on device -> Delete directly");
+		reset();
+	}
+}
 
 function close() {
 	$.nav.close();
