b546cdfc211ab9cfc07316b2c0c5b3c30ab7d2f0
Hans Knoechel <hans.knoechel@hs-osnabrueck.de>
2016-04-30 03:27:28 +0200

    Move Hyperloop-controls to alloy-syntax

--- app/widgets/com.appcelerator.robot.devicedetails/controllers/drive/index.js
+++ app/widgets/com.appcelerator.robot.devicedetails/controllers/drive/index.js
@@ -5,34 +5,7 @@ var robot;
  **/
 (function constructor(args) {
 	robot = args.robot;
-	setUI();
 })(arguments[0] || {});
-
-function setUI() {
-	var circle = Widget.createController("drive/circle").getView();
-	var top = Widget.createController("drive/arrow").getView();
-	var right = Widget.createController("drive/arrow").getView();
-	var bottom = Widget.createController("drive/arrow").getView();
-	var left = Widget.createController("drive/arrow").getView();
-
-	top.animate({
-		transform : Ti.UI.create2DMatrix().rotate(-90)
-	});
-
-	bottom.animate({
-		transform : Ti.UI.create2DMatrix().rotate(90)
-	});
-
-	left.animate({
-		transform : Ti.UI.create2DMatrix().rotate(180)
-	});
-
-	$.circle.add(circle);
-	$.arrowTop.add(top);
-	$.arrowRight.add(right);
-	$.arrowBottom.add(bottom);
-	$.arrowLeft.add(left);
-}
 
 function driveForward() {
 	drive(0, 0.5);
