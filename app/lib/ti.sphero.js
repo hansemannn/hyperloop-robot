7eda3475b2559334ab096106e8bcf08a8e3ef9d9
Hans Knoechel <hans.knoechel@hs-osnabrueck.de>
2016-05-10 23:59:09 +0200

    Use Sphero-SDK with Hyperloop (WiP)

--- app/lib/ti.sphero.js
+++ app/lib/ti.sphero.js
@@ -1,120 +1,213 @@
-var RKRobotDiscoveryAgent = require("RobotKit/RKRobotDiscoveryAgent");
+"use strict";
 
-/** 
- *  Private
- **/
- 
+// -- FRAMEWORKS
+
+var RKRobotDiscoveryAgent = require("RobotKit/RKRobotDiscoveryAgent"),
+    RKConvenienceRobot = require("RobotKit/RKConvenienceRobot"),
+    RKRobotConnecting = 10,//require("RobotCommandKit/RKRobotNotification").RKRobotConnecting,
+    RKRobotConnected = 20,//require("RobotCommandKit/RKRobotNotification").RKRobotConnected,
+    RKRobotOnline = 30,//require("RobotCommandKit/RKRobotNotification").RKRobotOnline,
+    RKRobotOffline = 40,//require("RobotCommandKit/RKRobotNotification").RKRobotOffline,
+    RKRobotDisconnected = 50,//require("RobotCommandKit/RKRobotNotification").RKRobotDisconnected,
+    RKRobotFailedConnect = 60,//require("RobotCommandKit/RKRobotNotification").RKRobotFailedConnect,
+    Robot = require("robot");
+
+// -- Private API's
+
 var events = [];
-var isDiscovering = false;
+var isSimulator = true//(Ti.App.getDeployType() == "development");
 
-var CONNECTION_STATUS_CONNECTING = 10;
-var CONNECTION_STATUS_CONNECTED = 20;
-var CONNECTION_STATUS_ONLINE = 30;
-var CONNECTION_STATUS_OFFLINE = 40;
-var CONNECTION_STATUS_DISCONNECTED = 50;
-var CONNECTION_STATUS_FAILED_CONNECT = 90;
+/**
+ * Robot connection process has started.
+ */
+var CONNECTION_STATUS_CONNECTING = RKRobotConnecting;
 
-var log = function(level, message) {
+/**
+ * Robot is connected, but not online.
+ */
+var CONNECTION_STATUS_CONNECTED = RKRobotConnected;
+
+/**
+ * Robot main processor is online and connected.
+ * This state is the one to watch for before sending commands.
+ */
+var CONNECTION_STATUS_ONLINE = RKRobotOnline;
+
+/**
+ * Robot main process is offline, but the radio is still connected. 
+ * Only available in BLE Robots.
+ */
+var CONNECTION_STATUS_OFFLINE = RKRobotOffline;
+
+/**
+ * Robot radio has disconnected - does not imply that that robot main processor is offline.
+ */
+var CONNECTION_STATUS_DISCONNECTED = RKRobotDisconnected;
+
+/**
+ *  Something went wrong during the connect cycle.
+ */
+var CONNECTION_STATUS_FAILED_CONNECT = RKRobotFailedConnect;
+
+/**
+ * Constructor
+ */
+(function constructor() {
+    if (isSimulator) {
+        log("info", "Initializing module ...");
+    } else {
+        var RKSubclass = Hyperloop.defineClass("RKSubclass", "NSObject");
+        
+        RKSubclass.addMethod({
+        	selector: 'onConnectionChange:',
+        	instance: true,
+        	arguments: ['NSNotification'],
+        	callback: function (notification) {
+        		handleRobotStateChangeNotification(notification);
+        	}
+        });
+        
+        var RKSubclassInstance = new RKSubclass();    
+        RKRobotDiscoveryAgent.sharedAgent().addNotificationObserverSelector(RKSubclassInstance, "onConnectionChange");
+    }   
+})();
+
+/**
+ * Handles a new notification being sent from the `RobotKit` framework 
+ * and sends out a Javascript event to the client.
+ * @param {NSNotification} notification The notification.
+ */
+function handleRobotStateChangeNotification(notification) {
+    Ti.API.info("Received state notification: ", notification.type);
+
+    switch (n.type) {
+    case RKRobotConnecting:
+        Ti.API.info("Connecting to robot (" + notification.robot.name + ")");
+    break;
+    case RKRobotOnline:
+        Ti.API.info("Connected to robot (" + notification.robot.name + ")");
+        break;
+    case RKRobotDisconnected:
+        Ti.API.info("Disconnected from robot (" + notification.robot.name + ")");
+        break;
+    default:
+        break;
+    }
+
+    fireEvent(notification);
+}
+
+// -- Utilities
+
+/**
+ * Prints a new log message in the client console.
+ * @param {String} level The log level, one of [debug, info, warn, error].
+ * @param {String} message The log message.
+ */
+function log(level, message) {
     Ti.API[level](message);
-};
+}
 
-var createRobot = function(args) {
-    var Robot = function(options) {
-        this.name = options.name;
-        this.identifier = options.identifier;
-    };
-    
-    Robot.prototype.startDrivingWithHeadingAndVelocity = function(heading, velocity) {
-        log("info", "Start driving with heading = " + heading + ", velocity = " + velocity);
-    };
-    
-    Robot.prototype.stopDriving = function() {
-        log("info", "Stop driving");
-    };
-    
-    Robot.prototype.setLEDColor = function(color) {
-        log("info", "Set LED color = " + color);
-    };
-    
-    Robot.prototype.setBackLEDBrightness = function(brightness) {
-        log("info", "Set LED brightness = " + brightness);
-    };
-    
-    Robot.prototype.resetHeading = function() {
-        log("info", "Reset heading");
-    };
-    
-    Robot.prototype.disconnect = function() {
-        log("info", "Disconnect");
-    };
-    
-    Robot.prototype.runMacro = function(macro) {
-        log("info", "Run macro");
-    };   
-    
-    return new Robot(args);
+/**
+ * Fires a new event to the Javascript client.
+ * @param {NSNotification} notification The notification.
+ */
+function fireEvent(notification) {
+    _.each(_.where(events, {
+        name: "connectionchange"
+    }), function(event) {
+        event.cb({
+            success: notification.type != RKRobotFailedConnect,
+            status: notification.type,
+            robot: new Robot(RKConvenienceRobot.convenienceWithRobot(notification.robot))
+        });
+    });
+}
+
+// -- Public API's
+
+/**
+ * Starts a new discovery and triggers a `connectionchange` event.
+ */
+exports.startDiscovery = function() {
+    if (isSimulator) {
+        log("info", "Start discovery ...");
+    } else {
+        RKRobotDiscoveryAgent.sharedAgent().startDiscovery();
+    }
 };
 
 /**
- *  Public
- **/
-exports.startDiscovery = function() {
-    log("info", "Start discovery");
-    isDiscovering = true;
-
-    setTimeout(function() {
-        _.each(_.where(events, {name: "connectionchange"}), function(event) {
-            log("info", "Found devices, firing events ...");
-            
-            event.cb({
-                success: true,
-                status: CONNECTION_STATUS_ONLINE,
-                robot: createRobot({
-                    name: "Test", 
-                    identifier: "demo-device",
-                    serialNumber: "1111-1111",
-                    online: true,
-                    connected: true,
-                    currentHeading: 90
-                })
-            });
-        })
-        isDiscovering = false;
-    }, 50);
-};
-
+ * Stops a current discovery and triggers a `connectionchange` event.
+ */
 exports.stopDiscovery = function() {
-    log("info", "Stop discovery");
-    isDiscovering = false;
+    if (isSimulator) {
+        log("info", "Start discovery ...");
+    } else {
+        RKRobotDiscoveryAgent.sharedAgent().stopDiscovery();
+    }
 };
 
+/**
+ * Returns whether or not the client is currently discovering robots.
+ * @return {Boolean} isDiscovering
+ */
 exports.isDiscovering = function() {
-    log("info", "Is discovering = " + isDiscovering);
-    return RKRobotDiscoveryAgent.sharedAgent().isDiscovering();
+    if (isSimulator) {
+        return false;
+    } else {
+        return RKRobotDiscoveryAgent.sharedAgent().isDiscovering();
+    }
 };
 
+/**
+ * Disconnects the client from his currently selected robot and triggers a `connectionchange` event.
+ */
 exports.disconnect = function() {
-    log("info", "Disconnect");
+    if (isSimulator) {
+        log("info", "Disconnect ...");
+    } else {
+        RKRobotDiscoveryAgent.sharedAgent().disconnect();
+    }
 };
 
+/**
+ * Disconnects all currently selected robots from its clients and triggers a `connectionchange` event.
+ */
 exports.disconnectAll = function() {
-    log("info", "Disconnect all");    
+    if (isSimulator) {
+        log("info", "Disconnect all ...");
+    } else {
+        RKRobotDiscoveryAgent.sharedAgent().disconnectAll();
+    }
 };
 
+/**
+ * Creates a new event listener to emulate the legacy behavior of the Titanium proxy-system.
+ * @param {String} _name The event name.
+ * @param {Callback} _name The event callback.
+ */
 exports.addEventListener = function(_name, _cb) {
     log("info", "Adding event listener = " + _name);
     events.push({
-        name: _name, 
+        name: _name,
         cb: _cb
     });
 };
 
+/**
+ * Removes an existing event listener to emulate the legacy behavior of the Titanium proxy-system.
+ * @param {String} _name The event name.
+ * @param {Callback} _name The event callback.
+ */
 exports.removeEventListener = function(_name, _cb) {
     log("info", "Removing event listener = " + _name);
-    events = _.reject(events, function(event) { 
-        return event.name === _name; 
+    events = _.reject(events, function(event) {
+        return event.name === _name;
     });
 };
+
+// -- Public constants
 
 exports.CONNECTION_STATUS_CONNECTING = CONNECTION_STATUS_CONNECTING;
 exports.CONNECTION_STATUS_CONNECTED = CONNECTION_STATUS_CONNECTED;
